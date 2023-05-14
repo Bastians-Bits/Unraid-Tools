5e2426c9b87b77804f59757cbb7dd01510b5a9e9
Bastian <51163929+Bastians-Bits@users.noreply.github.com>
2023-05-14 08:31:11 +0200

    Update generator_windows.sh

--- Server/generator_windows.sh
+++ Server/generator_windows.sh
@@ -1,37 +1,29 @@
-#!/bin/bash
-###############################################################################
-# gen-mount-win.sh
-#
-# Reads the unraid config and create a Windows Batch Script to mount all
-# user shares
-#
-# Before the first usage:
-# - Read the script and make sure you understand what id does.
-#   Never execute code you don't fully understand on your machine!
-# - After that, replace the server and username variables with the server name
-#   or IP and the user to create the script for
-#
-###############################################################################
+#name=Mount Script Windows
+#description=Create a batch script to mount all shares in Windows
+#arrayStarted=true
+#argumentDescription=Server Address? Username?
 
-server=
-username=
+args=( $1 )
+echo "Test ${#args[@]}"
+if [ "${#args[@]}" -ne "2" ]; then
+   echo "Wrong amount of arguments, exit"
+   exit 1
+fi
 
-##
-## Template for the header
-##
+server=${1}
+username=${2}
+
 header=$(cat <<EOF
-###################
-### COPY FROM HERE:
-###################
+
+--- COPY FROM HERE:
+-------------------------------------------------------------------------------
+
 @echo off
 set /p password="Password: "
 
 EOF
 )
 
-##
-## Template for the body
-##
 template=$(cat <<EOF
 
 
@@ -47,46 +39,37 @@ if exist \$letter:\ (
 EOF
 )
 
-##
-## Template for the footer
-##
 footer=$(cat <<EOF
 
 pause
 @echo on
-###################
-### TO HERE
-###################
+
+-------------------------------------------------------------------------------
+--- TO HERE
 EOF
 )
 
-# Iterate each share config
 for file in /boot/config/shares/*.cfg; do
-   # Get the share name from the filename
+   # Check if user in write list
    share=${file%.cfg}; share=${share##*/}; export share=${share,,};
 
-   # Check if the share is exported
    is_export=$(grep -e "shareExport=.*" $file)
    [[ $is_export =~ shareExport=\"(.*{1})\" ]]
    is_export=${BASH_REMATCH[1]}
+
    if [ "$is_export" != "e" ]; then continue; fi
 
-   # Check if the user is either in the read or write list
    if grep -q -e "shareReadList=\".*$username.*\"" $file || grep -q -e "shareWriteList=\".*$username.*\"" $file; then
-      # Get the letter from the share description
       letter=$(grep -e "shareComment=.*mountpoint.*" $file)
       [[ $letter =~ \(mountpoint\ (.*{1})\) ]]
       export letter=${BASH_REMATCH[1]}
 
-      # Check if the letter is set
       if [[ -z "${letter+x}" ]]; then continue; fi
 
-      # Build the body from the template
       main=$(echo -n "$main$template" | perl -pe 's/\$(\{)?([a-zA-Z_]\w*)(?(1)\})/$ENV{$2}/g')
    fi
 done
 
-# Echo the script
 echo "$header"
 echo "$main"
 echo "$footer"
